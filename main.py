aa459e62c0a760ef47d4bd168081cf9c52a148b2
Moises <moises@pluio.com>
2018-09-12 07:11:22 -0400

    Housekeeping here and there.

--- main.py
+++ main.py
@@ -16,17 +16,16 @@ Use this script if you know what you are doing and at your own risk.
 '''
 
 config = ConfigParser()
-q = os.getcwd() + os.path.sep + 'config.ini'
-config.read(q)
+iso = os.getcwd() + os.path.sep + 'config.ini'
+config.read(iso)
 torpath = config.get('tor', 'torpath')
 torrc = config.get('tor', 'torrc')
 if not torpath or not torrc:
-    sys.exit('Missing configuration in < %s >. Aborting.' %q)
+    sys.exit('Missing configuration in < %s >. Aborting.' %iso)
 
-c = []
-p = []
-m = False
-v = r'https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2'
+stdin = []
+prg = []
+see = r'https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2'
 
 for arg in sys.argv[1:]:
     cd = []
@@ -36,8 +35,8 @@ for arg in sys.argv[1:]:
     ftl = [v for sublt in cd for v in sublt]
     if not arg.upper() in [s.strip('\n') for s in ftl]:
         sys.exit('< ' + arg + ' > not a valid country code. See %s for a list' \
-        ' of country codes. Aborting.' %v)
-    c.append(arg.lower())
+        ' of country codes. Aborting.' %see)
+    stdin.append(arg.lower())
 
 rc = torpath + os.path.sep + torrc + os.path.sep + 'torrc'
 if os.path.exists(rc):
@@ -48,26 +47,26 @@ if os.path.exists(rc):
             elif 'StrictNodes' in l:
                 pass
             else:
-                p.append(l)
+                prg.append(l)
 else:
     sys.exit('Missing configuration or wrong values or' \
      ' < %s > lacks proper permissions. Aborting.' %rc)
 
-if len(c) > 1:
+if len(stdin) > 1:
     o = 'ExitNodes '
-    for i in c:
+    for i in stdin:
         o +='{%s},' % i
-    p.append(o.rstrip(',') + '\n')
+    prg.append(o.rstrip(',') + '\n')
 else:
     o = 'ExitNodes {%s}\n'
-    p.append(o %c[0])
-    p.append('StrictNodes 1\n')
+    prg.append(o %stdin[0])
+    prg.append('StrictNodes 1\n')
 
 ed = os.path.getmtime(rc)
 now = datetime.datetime.fromtimestamp(ed).strftime("%b%d%Y%H")
 copyfile(rc, rc + '.bk.' + now)
 
 with open(rc, "w") as f:
-    for l in p:
+    for l in prg:
         f.write(l)
 sys.exit(0)
